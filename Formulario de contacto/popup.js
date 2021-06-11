fcac99babc0c55946c7e5945bf59feb5768b7906
Robert Valera Palomino <72207145@certus.edu.pe>
2021-06-11 14:33:30 -0500

    cambios 11/06/21

--- Formulario de contacto/popup.js
+++ Formulario de contacto/popup.js
@@ -1,3 +1,18 @@
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
 const nombre = document.getElementById("name")
 const email = document.getElementById("email")
 const pass = document.getElementById("password")
@@ -29,3 +44,4 @@ form.addEventListener("submit", e=>{
         parrafo.innerHTML = "Enviado"
     }
 })
+
